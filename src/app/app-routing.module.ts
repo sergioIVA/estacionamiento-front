fb6bab022dce68494534a708cec61822732ee31e
Sergio Ivan Villamizar Alvarado - Ceiba Software <sergio.villamizar@ceiba.com.co>
2019-06-28 16:28:23 -0500

    ultimo comiit viernes seleccion colombia vs chile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListarPuestosComponent } from './feature/puesto/listar-puestos/listar-puestos.component';
 import { ListarVehiculoComponent } from './feature/vehiculo/listar-vehiculo/listar-vehiculo.component';
+import { CrearVehiculoComponent } from './feature/vehiculo/crear-vehiculo/crear-vehiculo.component';
 
 const routes: Routes = [
   {path:'puestos',component:ListarPuestosComponent},
-  {path:'vehiculos',component:ListarVehiculoComponent}
+  {path:'vehiculos',component:ListarVehiculoComponent},
+  {path:'crearVehiculo',component:CrearVehiculoComponent}
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
